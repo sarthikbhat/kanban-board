3ed47e0006d0cda1c5807cb9fa582a724f68321e
sarthikbhat <bhatsarthik28@gmail.com>
2023-12-29 12:56:02 +0530

    MVP ready to deploy - 3

--- client/src/app/(auth)/layout.tsx
+++ client/src/app/(auth)/layout.tsx
@@ -5,12 +5,11 @@ export const metadata: Metadata = {
   title: "Kanban - Auth",
 };
 
-interface ILayout {
-  path: string;
+export default function RootLayout({
+  children,
+}: {
   children: React.ReactNode;
-}
-
-export default function RootLayout({ children }: ILayout) {
+}) {
   return (
     <section className="flex">
       <section className="flex-1 bg-[#395886]" style={{flex:'1.5'}}><AuthSide/></section>
