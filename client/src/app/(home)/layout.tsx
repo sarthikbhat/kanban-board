6f012333a03405e2c8c53486e6c5240c0556c9b0
sarthikbhat <bhatsarthik28@gmail.com>
2024-05-26 19:25:55 +0530

    test SSR og tags

--- client/src/app/(home)/layout.tsx
+++ client/src/app/(home)/layout.tsx
@@ -1,14 +1,25 @@
-'use client';
 import Navbar from '@/components/Navbar';
 import Layout from '@/components/Layout';
 import './styles.css';
+import { Metadata } from 'next';
 
 interface ILayout {
   children: React.ReactNode;
 }
 
-export default function RootLayout({ children }: ILayout) {
+export const metadata: Metadata = {
+  openGraph: {
+    title: 'Flashback',
+    description: 'Test Description',
+    images: [
+      {
+        url: 'https://flashbackimagesthumbnail.s3.ap-south-1.amazonaws.com/Aarthi_Vinay_19122021/Ec_E__DSC1682.jpg'
+      }
+    ]
+  }
+};
 
+export default function RootLayout({ children }: ILayout) {
   return (
     <>
       <Layout>
